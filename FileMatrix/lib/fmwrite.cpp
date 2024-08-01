fe4ba82573317c7130894213977a4afe8f452b43
Moligarch <mr.verkiani@gmail.com>
2024-08-01 05:15:11 +0430

    [UPD] fix misplaced file in project folders

---
+++ FileMatrix/lib/fmwrite.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "FileMatrix/fmwrite.h"
+
+
+
+namespace fmatrix
+{
+    namespace write
+    {
+    } //namespace write
+
+    namespace mtwrite //multi-thread write
+    {
+    } //namespace mtwrite
+}
